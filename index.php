ef2d2b60c31d22ad38addea1ff7eb3d5a3a93bc1
KAAAsS <admin@kaaass.net>
2016-03-06 12:12:22 +0800

    Remove xajax requiring.

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 require 'config.php';
 require 'function.php';
-if (!isAuth ()) {
+if (isset ( $_POST ["check"] )) {
+	if ($_POST ["check"] = 'true') {
+		echo 'Checking...'.time();
+		exit();
+	}
+}
+if (! isAuth ()) {
 	require_once './lib/phpqrcode.php';
-	require_once './lib/xajax_core/xajax.inc.php';
 	$authkey = getRandChar ( 16 );
 	$qrencoded = authcode ( $qrdomain . '@' . $authkey, '', $encode_key, $qrtime );
 	QRcode::png ( $qrencoded, "qrcode.png", 'L', 4, 3 );
-	$xajax = new xajax();
-	$xajax->configure('javascript URI', './lib/');
-	$xajax->register(XAJAX_FUNCTION, "haslogin");
-	function haslogin() {
-		$obj = new xajaxResponse();
-		$obj->assign("status", "innerHTML", "Posting...time()");
-		return $obj;
-	}
+} else {
+	echo '<p>' . authcode ( $_POST ["akey"], 'DECODE', $encode_key ) . '</p>';
+	exit();
 }
 function isAuth() {
 	// return isset ( $_GET ["akey"] ) & isset ( $_GET ["ukey"] ) & isset ( $_GET ["code"] );
@@ -26,21 +26,22 @@ function isAuth() {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Login</title>
-<?php
-if (!isAuth ()) {
-	$xajax->printJavascript();
-	echo '<script type="text/javascript" charset="UTF-8">window.setInterval(xajax_haslogin(), 5000);</script>';
-}
-?>
+<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" charset="UTF-8">
+	$(document).ready(function(){  
+		setInterval(function(){
+			$.post("<?php echo $_SERVER['PHP_SELF']; ?>","check=true",function(result){
+				var a=document.getElementById ("status");
+		        a.innerHTML = result;
+			});
+		}, 5000);  
+	});  
+</script>
 </head>
 <body>
 <?php
-if (isAuth ()) {
-	echo '<p>'.authcode ( $_POST ["akey"], 'DECODE', $encode_key ).'</p>';
-} else {
 	echo '<img src="qrcode.png">';
 	echo "<p>$qrencoded</p>";
-}
 ?>
 <span id="status">Waiting...</span>
 </body>
